1531293157f543ed5ceb337297c1fbd5c9a14b42
ejung <leejungstyle@gmail.com>
2019-12-01 22:35:46 +0900

    typescript 함수 정의 및 호출 예제

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('test');
+const name = "ejung",
+    age = 34,
+    gender = "female";
+
+const sayHi = (name, age, gender) => { //gender? 로 처리하면 함수 호출 시 gender 파라미터값을 정의하지 않아도 정상 실행됨
+    console.log(`Hello ${name} you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age, gender);
+export {};
