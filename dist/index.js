fbb605cdba83db412a03dc8e3d483c19d08f348b
ejung <leejungstyle@gmail.com>
2019-12-02 01:12:47 +0900

    class on typescript

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "ejung",
-    age: 34,
-    gender: "female"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const ejung = new Human("Ejung", 34, "Female");
 const sayHi = (person) => {
     return (`Hello ${person.name} you are ${person.age}, you are a ${person.gender}`);
 };
-var sayHiPrint = sayHi(person);
+var sayHiPrint = sayHi(ejung);
 console.log(sayHiPrint);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name : string;
-    age : number;
-    gender : string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender:string;
+    constructor (name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender=gender;
+    }
 }
 
-const person = {
-    name : "ejung",
-    age : 34,
-    gender : "female"
-}
+const ejung = new Human("Ejung", 34, "Female");
 
 const sayHi = (person:Human):string => {
     return(`Hello ${person.name} you are ${person.age}, you are a ${person.gender}`);
 }
 
-var sayHiPrint = sayHi(person);
+var sayHiPrint = sayHi(ejung);
 console.log(sayHiPrint);
 export {};
